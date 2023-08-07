ae3dd48ec623f8994654163d49d28c30e30a788c
Yash Liladhar Amarapurkar <73273674+Yash19-Yash19@users.noreply.github.com>
2023-08-07 13:19:03 +0530

    java 4th practical
    2nd exp upto
    border layout half done

--- sem 5/sem 5/Advanced Java Programming/Exp2/testlyt2.java
+++ sem 5/sem 5/Advanced Java Programming/Exp2/testlyt2.java
@@ -1,26 +1,26 @@
-import java.awt.*;
+// import java.awt.*;
 
-public class testlyt2 extends Frame {
+// public class testlyt2 extends Frame {
 
-    testlyt2() {
+//     testlyt2() {
 
-        Button b1 = new Button("NORTH");
-        Button b2 = new Button("SOUTH");
-        Button b3 = new Button("EAST");
-        Button b4 = new Button("WEST");
-        Button b5 = new Button("CENTER");
+//         Button b1 = new Button("NORTH");
+//         Button b2 = new Button("SOUTH");
+//         Button b3 = new Button("EAST");
+//         Button b4 = new Button("WEST");
+//         Button b5 = new Button("CENTER");
 
-        add(b1, BorderLayout.NORTH);
-        add(b2, BorderLayout.SOUTH);
-        add(b3, BorderLayout.EAST);
-        add(b4, BorderLayout.WEST);
-        add(b5, BorderLayout.CENTER);
+//         add(b1, BorderLayout.NORTH);
+//         add(b2, BorderLayout.SOUTH);
+//         add(b3, BorderLayout.EAST);
+//         add(b4, BorderLayout.WEST);
+//         add(b5, BorderLayout.CENTER);
 
-        setSize(300, 300);
-        setVisible(true);
-    }
+//         setSize(300, 300);
+//         setVisible(true);
+//     }
 
-    public static void main(String[] args) {
-        new testlyt2();
-    }
-}
+//     public static void main(String[] args) {
+//         new testlyt2();
+//     }
+// }
