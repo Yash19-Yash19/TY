6862d8e78a627b65bb36239fb247f639f0f5e52d
Yash Liladhar Amarapurkar <73273674+Yash19-Yash19@users.noreply.github.com>
2023-09-03 00:53:47 +0530

    18

--- sem 5/sem 5/Advanced Java Programming/Exp 5/demon10.java
+++ sem 5/sem 5/Advanced Java Programming/Exp 5/demon10.java
@@ -1,19 +1,30 @@
-// import java.awt.event.MouseAdapter;
+// write a code for  adapter class of mousemotion interface in java frame
+import java.awt.*;
+import java.awt.event.*;
+class demon10 extends Frame implements MouseMotionListener
+{
+    demon10()
+    {
+        addMouseMotionListener(this);
+        setSize(400,400);
+        setLayout(null);
+        setVisible(true);
+    }
+    public void mouseDragged(MouseEvent e)
+    {
+        Graphics g=getGraphics();
+        g.setColor(Color.red);
+        g.fillOval(e.getX(),e.getY(),20,20);
+    }
+    public void mouseMoved(MouseEvent e)
+    {
+        Graphics g=getGraphics();
+        g.setColor(Color.blue);
+        g.fillOval(e.getX(),e.getY(),20,20);
+    }
+    public static void main(String args[])
+    {
+        new demon10();
+    }
+}
 
-// import javax.swing.*;
-
-// class demon10 extends MouseAdapter {
-// JFrame f;
-
-// demon10() {
-// f = new JFrame();
-// f.setSize(500, 500);
-// f.setVisible(true);
-// f.setLayout(null);
-// f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-// f.addMouseListener(this);
-// }
-
-// public static void main(String[] args) {
-// new demon10();
-// }
